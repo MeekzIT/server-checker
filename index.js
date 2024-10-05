a02fed436bae6582db71da75fe0a2b2f3756bfd8
viktorya_ <52004572+SargisGasparyan@users.noreply.github.com>
2024-10-05 14:34:08 +0400

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
+var cron = require("node-cron")
 
 const token = '8061745686:AAFboXuCtQSJyJbM1BBW-qStxMzvb3IDQ94';
 const chatId = '1338222648';
@@ -25,10 +26,28 @@ async function checkAPIs() {
         bot.sendMessage(chatId, `API call failed: ${error.message}`);
     }
 }
+async function getAll() {
+    try {
+        const response1 = await axios.get('http://154.49.137.44:2000/api/v1/devices');
+        if (response1.status === 200) {
+            console.log(JSON.stringify(response1.data, null, 2), "response1");
+
+            for (const device of response1.data) {
+                const message = `Device ID: ${device.p2}, Info: ${JSON.stringify(device, null, 2)}`;
+                await bot.sendMessage(chatId, message + ' history');
+            }
+        }
+    } catch (error) {
+        console.error('HISTORY API call failed:', error.message);
+    }
+}
+
+cron.schedule("0 23 * * *", () => {
+    getAll()
+})
 
 setInterval(checkAPIs, 1200000);
 
-// Optional: Handle direct messages from you (for logging or testing purposes)
 bot.on('message', (msg) => {
     const receivedChatId = msg.chat.id;
     console.log(`Received message from chat ${receivedChatId}:`, msg.text);
